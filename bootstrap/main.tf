a414f1f1a050f362db4ec53d811c52374b13a556
Raphael <raphael.danjard@hotmail.com>
2026-05-07 16:57:09 +0200

    fix : sécu

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -10,12 +10,13 @@ resource "aws_s3_bucket_versioning" "tfstate" {
   }
 }
 
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
   bucket = aws_s3_bucket.tfstate.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256" #tfsec:ignore:aws-s3-encryption-customer-key
+      sse_algorithm = "AES256"
     }
   }
 }
--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -21,12 +21,13 @@ resource "aws_s3_bucket_versioning" "main" {
   }
 }
 
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   bucket = aws_s3_bucket.main.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256" #tfsec:ignore:aws-s3-encryption-customer-key
+      sse_algorithm = "AES256"
     }
   }
 }
